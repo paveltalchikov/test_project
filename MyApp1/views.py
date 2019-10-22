59f048c8747be342ebad0e7c433f7f1caacfdefa
Pavel Talchikov <pavel.talchikov@gmail.com>
2019-10-23 02:51:02 +0300

    time amd html

--- MyApp1/views.py
+++ MyApp1/views.py
@@ -1,6 +1,19 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    return render(
+        request,
+        "MyApp1/index.html",
+            {
+                'title' : "Hi Jango",
+                'message' : "hello Jango",
+                'content': " performed on " + now.strftime("%A, %d %B, %Y at %X")
+            }
+        )
